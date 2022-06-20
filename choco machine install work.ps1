d456bbe39e9921093f76ee5f49c5d11541892ec1
kylebraham <kyl3.david@gmail.com>
2022-06-20 17:49:06 +0100

    Update choco machine install work.ps1
    - add google chrome
    - add vscode extensions section

--- choco machine install work.ps1
+++ choco machine install work.ps1
@@ -1,10 +1,14 @@
+#########################
+##### Install WSL
+#########################
+wsl --install
+
+#########################
+##### Chocolatey packages
+#########################
 ## Install choco
 #Set-ExecutionPolicy AllSigned
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-
-## Install WSL
-wsl --install
 
 ## Coding
 choco install git -y
@@ -18,6 +22,7 @@ choco install microsoft-teams -y
 
 ## Internet
 choco install brave -y
+choco install googlechrome -y
 
 ## Tools
 choco install 7zip -y 
@@ -28,3 +33,25 @@ choco install devtoys -y
 
 ## Activity tracking
 choco install activitywatch -y
+
+#########################
+##### Node JS packages
+#########################
+npm install -g @angular/cli
+# set execution policy for ng to work in powershell
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
+
+
+#########################
+##### VSCode extensions
+#########################
+
+# Spell checker
+code --install-extension streetsidesoftware.code-spell-checker
+## Draw.io Integration
+#code --install-extension hediet.vscode-drawio
+# Angular Language Service
+code --install-extension Angular.ng-template
+
+code --list-extensions
+
