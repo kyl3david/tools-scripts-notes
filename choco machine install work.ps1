a1df6071b4e978ddd1f8cd7f463dcb179128a84d
kyleabraham <kyl3.david@gmail.com>
2022-11-09 08:49:22 +0000

    feat: add missing packages
    Add missing choco and vscode extension packages

--- choco machine install work.ps1
+++ choco machine install work.ps1
@@ -16,6 +16,7 @@ choco install postman -y
 choco install vscode -y
 choco install nodejs -y
 choco install notepadplusplus -y
+# choco install drawio -y
 
 ## Comms
 choco install microsoft-teams -y
@@ -27,11 +28,24 @@ choco install googlechrome -y
 ## Tools
 choco install 7zip -y 
 choco install adobereader -y
-choco install sharex -y
+# choco install sharex -y
 ## cross-platfornm alternative to xshare is flameshot
-# choco install flameshot -y
+choco install flameshot -y
 choco install powertoys -y
 choco install devtoys -y
+### Database tools
+choco install dbeaver -y
+choco install sql-server-management-studio -y
+### Docker
+# choco install docker-desktop -y
+### Recording
+choco install obs-studio -y
+### Virtualization
+# choco install virtualbox -y
+
+## dotnet
+### this will install the lastest version, to get other dotnet versions you will need to specify the version e.g. .NET 5 or .NET 6
+choco install dotnet-sdk -y
 
 ## Activity tracking
 choco install activitywatch -y
@@ -56,6 +70,19 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension Angular.ng-template
 # Prettier - Code formatter
 code --install-extension esbenp.prettier-vscode
+# Code Spell Checker
+code --install-extension streetsidesoftware.code-spell-checker
+# Dev Containers
+code --install-extension ms-vscode-remote.remote-container
+# Docker
+code --install-extension ms-azuretools.vscode-docker
+# PowerShell
+code --install-extension ms-vscode.PowerShell
+# Rainbow CSV
+code --install-extension mechatroner.rainbow-csv
+# Excel Viewer
+code --install-extension GrapeCity.gc-excelviewer
+# Markdown Preview Mermaid Support
+code --install-extension bierner.markdown-mermaid
 
 code --list-extensions
-
